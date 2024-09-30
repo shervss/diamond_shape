efadac601cec73d1eec732eb04d30271bbbf1f76
Shervin Mangulabnan <163648798+shervss@users.noreply.github.com>
2024-09-30 13:04:52 +0800

    add pseudocode

---
+++ diamond_shape.py
@@ -0,0 +1,8 @@
+# Program by: Shervin Marco Mangulabnan 
+# Date of Creation: September 26, 2024
+# Purpose: A program that generates a diamond shape using the "*" character, based on an odd integer "n".
+
+# Check if n is an odd integer
+# Calculate the number of rows for the top half and the bottom half
+# Print the top half of the diamond
+# Print the bottom half of the diamond
